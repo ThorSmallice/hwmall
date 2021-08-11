595d55b85e3b369c18f82cdc92d2bd5696890b2e
DB-Home <912356847@qq.com>
2021-08-12 00:08:32 +0800

    创建主页

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,16 @@
+<!-- 头部导航栏  -->
+<template>
+    <div id="headerBar">
+        头部导航栏
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,54 @@
+<template>
+    <div id="Home"> 
+        <header>
+            <header-bar>
+
+            </header-bar>
+        </header>
+
+        <main>
+
+        </main>
+
+        <footer>
+
+        </footer>
+    </div>
+</template>
+
+<script>
+import headerBar from "../components/HeaderBar.vue";
+export default {
+    components: {
+        headerBar
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+#Home{
+    .header-adve {
+        width: 100%;
+        overflow: hidden;
+        .header-img{
+            height: 90px;
+            position: relative;
+            width: 1200px;
+            margin: 0 auto;
+            background-color: pink;
+            a {
+                position: relative;
+                left: 50%;
+                margin-left: -960px;
+                display: block;
+                text-align: center;
+                width: 1920px;
+                height: 90px;
+                img {
+                    vertical-align: top;
+                }
+            }
+        }
+    }
+}
+</style>
